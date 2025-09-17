bc42a6cd77992b4265b00a8bc642f3377821a235
kondeti shivaji <shivajikondeti555@gmail.com>
2025-09-17 17:20:45 +0530

    test ci pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello, This is Simple React application with Vite..</h1>
+      {/* <h1>Hello, This is Simple React application with Vite..</h1> */}
       <div>
         <br />
         <p>
