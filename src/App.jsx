fd1a849e0ad39c1bae5a84a15998dcbbf2ae2872
kondeti shivaji <shivajikondeti555@gmail.com>
2025-09-17 17:16:48 +0530

    test ci pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ function App() {
   return (
     <>
       <h1>Hello, This is Simple React application with Vite..</h1>
+      <div>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          expedita accusamus provident asperiores tempore aut maiores, inventore
+          assumenda obcaecati earum saepe, iusto id enim! Nesciunt iste ullam
+          culpa consequuntur voluptatibus.
+        </p>
+      </div>
     </>
   );
 }
