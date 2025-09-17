07395bec2a13a59073c0df5a3f5edde03256225b
kondeti shivaji <shivajikondeti555@gmail.com>
2025-09-17 19:23:10 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,35 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      {/* <h1>Hello, This is Simple React application with Vite..</h1> */}
-      <div>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-          expedita accusamus provident asperiores tempore aut maiores, inventore
-          assumenda obcaecati earum saepe, iusto id enim! Nesciunt iste ullam
-          culpa consequuntur voluptatibus.
-        </p>
-      </div>
-    </>
+    <div id="section">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        expedita accusamus provident asperiores tempore aut maiores, inventore
+        assumenda obcaecati earum saepe, iusto id enim! Nesciunt iste ullam
+        culpa consequuntur voluptatibus. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Numquam expedita accusamus provident asperiores
+        tempore aut maiores, inventore assumenda obcaecati earum saepe, iusto id
+        enim! Nesciunt iste ullam culpa consequuntur voluptatibus. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Numquam expedita accusamus
+        provident asperiores tempore aut maiores, inventore assumenda obcaecati
+        earum saepe, iusto id enim! Nesciunt iste ullam culpa consequuntur
+        voluptatibus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        expedita accusamus provident asperiores tempore aut maiores, inventore
+        assumenda obcaecati earum saepe, iusto id enim! Nesciunt iste ullam
+        culpa consequuntur voluptatibus. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Numquam expedita accusamus provident asperiores
+        tempore aut maiores, inventore assumenda obcaecati earum saepe, iusto id
+        enim! Nesciunt iste ullam culpa consequuntur voluptatibus. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Numquam expedita accusamus
+        provident asperiores tempore aut maiores, inventore assumenda obcaecati
+        earum saepe, iusto id enim! Nesciunt iste ullam culpa consequuntur
+        voluptatibus.
+      </p>
+      <br />
+    </div>
   );
 }
 
